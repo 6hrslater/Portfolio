f99da77cdc5db0b5b0eb244a2975dfc9d4912885
Darian Chen <darian.chen8639@gmail.com>
2022-11-17 17:28:57 -0800

    added missing icon and redirects

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin, AiFillMessage } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
 import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
@@ -38,6 +38,9 @@ const Header = () => (
       </SocialIcons>
       <SocialIcons href="https://www.linkedin.com/in/dchen1999/">
         <AiFillLinkedin size="3rem"></AiFillLinkedin>
+      </SocialIcons>
+      <SocialIcons href="/">
+        <AiFillMessage size="3rem"></AiFillMessage>
       </SocialIcons>
     </Div3>
 
